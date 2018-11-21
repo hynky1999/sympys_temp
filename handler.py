a917313233898cd21ccd27fad410fa356e8cdc19
Alessandro <alessandro.mari.deer@gmail.com>
2018-11-22 03:30:03 +0400

    handler done for with checks.py

--- handler.py
+++ handler.py
@@ -1,24 +1,43 @@
+import sys
 import json
-
+from checks import equiv_symbolic, string_match, is_expanded
+from checks import parse_checks, check_func
 
 def handle(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
+    body = event["body"]
+
+    if body["input"] is None or\
+       body["expected"] is None or\
+       body["checks"] is None:
+        return {
+            "statusCode": 400,
+            "body": {
+                "message": "Bad Request"
+            }
+        }
+    
+    checks = parse_checks(body["checks"])
+    result = "true"
+    for check in checks:
+        value = check_func[check](body["input"], body["expected"], checks[check])
+        if value != "true":
+            result = value
+            break
 
     response = {
         "statusCode": 200,
-        "body": json.dumps(body)
+        "body": json.dumps({
+            "result": result
+        })
     }
 
     return response
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
+# res = handle({
+#     "body": {
+#         "input": "\\frac{1}{2}",
+#         "expected": "0.5",
+#         "checks": "equivLiteral"
+#     }
+# }, {})
+# print(res)
