a9f43135dd7b2beed76259996b3db6c103913636
Alessandro <alessandro.mari.deer@gmail.com>
2018-12-07 03:34:43 +0400

    exception handler added for test endpoint

--- handler.py
+++ handler.py
@@ -71,33 +71,46 @@ def handle(event, context):
 def test(event, context):
     test_file_name = 'tests.csv'
 
-    test_count, fail_count, fails = 0, 0, []
-    with open(test_file_name, 'r', encoding='utf-8') as test_file:
-        for row in csv.reader(test_file):
-            testno, desc, input_latex, expected_latex, options, expected_result = row
-            main, add = parse_checks(options).popitem()
+    try:
+        test_count, fail_count, fails = 0, 0, []
+        with open(test_file_name, 'r', encoding='utf-8') as test_file:
+            for row in csv.reader(test_file):
+                testno, desc, input_latex, expected_latex, options, expected_result = row
+                main, add = parse_checks(options).popitem()
 
-            #print(testno, expected_result, sep='\t')
-            test_result = check_func[main](input_latex, expected_latex=expected_latex, options=add)
-            if test_result != expected_result:
-                fails.append(row + [test_result])
-                fail_count += 1
-            test_count += 1
-    
-    report_str = '{} checks passed, {} failed\n'.format(test_count - fail_count, fail_count)
-    for testno, desc, input_latex, expected_latex, options, expected_result, result in fails:
-        report_str += '{}\t{}: '.format(testno, desc)
-        report_str += 'expected "{}", got "{}"\n'.format(expected_result, result)
+                #print(testno, expected_result, sep='\t')
+                test_result = check_func[main](input_latex, expected_latex=expected_latex, options=add)
+                if test_result != expected_result:
+                    fails.append(row + [test_result])
+                    fail_count += 1
+                test_count += 1
+        
+        report_str = '{} checks passed, {} failed\n'.format(test_count - fail_count, fail_count)
+        for testno, desc, input_latex, expected_latex, options, expected_result, result in fails:
+            report_str += '{}\t{}: '.format(testno, desc)
+            report_str += 'expected "{}", got "{}"\n'.format(expected_result, result)
 
-    response = {
-        "statusCode": 200,
-        "headers": {
-            "Access-Control-Allow-Origin": "*"
-        },
-        "body": report_str
-    }
+        response = {
+            "statusCode": 200,
+            "headers": {
+                "Access-Control-Allow-Origin": "*"
+            },
+            "body": report_str
+        }
 
-    return response
+        return response
+    except Exception as e:
+        response = {
+            "statusCode": 500,
+            "headers": {
+                "Access-Control-Allow-Origin": "*"
+            },
+            "body": json.dumps({
+                "error": "Internal Server Error",
+                "message": str(e)
+            })
+        }
+        return response
 
 # res = handle({
 #     "body": "{\"input\": \"\\\\frac{1}{2}\", \"expected\": \"0.5\", \"checks\": \"equivLiteral:\"}"
