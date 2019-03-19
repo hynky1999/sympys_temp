6d9862c4ed78bd026561d0dd0d5877fcbe6c4713
Lawrence <lawrencechernin@yahoo.com>
2019-03-19 15:17:46 -0700

    Hynek fixed the handler so it now considers all chained checks, not just the last one. This issue was caught by new test #282 which has three chained checks.

--- handler.py
+++ handler.py
@@ -2,7 +2,7 @@ import csv
 import sys
 import json
 
-from checks import parse_checks, check_func, replace_variables, calculate_expression
+from checks import parse_checks, check_func, is_simplified
 
 def handle(event, context):
     print("Request Body: ")
@@ -67,65 +67,6 @@ def handle(event, context):
         }
         return response
 
-def calculate(event, context):
-    try:
-        body = json.loads(event["body"])
-    except Exception as e:
-        response = {
-            "statusCode": 500,
-            "headers": {
-                "Access-Control-Allow-Origin": "*"
-            },
-            "body": json.dumps({
-                "error": "Error occured",
-                "message": str(e)
-            })
-        }
-        return response
-
-    if body["latex"] is None or\
-       body["variables"] is None:
-        return {
-            "statusCode": 400,
-            "headers": {
-                "Access-Control-Allow-Origin": "*"
-            },
-            "body": {
-                "message": "Bad Request"
-            }
-        }
-    try:
-        input_latex = body["latex"]
-        variables = body["variables"]
-
-        input_latex = replace_variables(input_latex, variables)
-        result = calculate_expression(input_latex)
-
-        response = {
-            "statusCode": 200,
-            "headers": {
-                "Access-Control-Allow-Origin": "*"
-            },
-            "body": json.dumps({
-                "result": result
-            })
-        }
-
-        return response
-    except Exception as e:
-        print(e)
-        response = {
-            "statusCode": 500,
-            "headers": {
-                "Access-Control-Allow-Origin": "*"
-            },
-            "body": json.dumps({
-                "error": "Internal Server Error",
-                "message": str(e)
-            })
-        }
-        return response
-
 def test(event, context):
     test_file_name = 'tests.csv'
 
@@ -134,13 +75,18 @@ def test(event, context):
         with open(test_file_name, 'r', encoding='utf-8') as test_file:
             for row in csv.reader(test_file):
                 testno, desc, input_latex, expected_latex, options, expected_result = row
-                main, add = parse_checks(options).popitem()
-
-                #print(testno, expected_result, sep='\t')
-                test_result = check_func[main](input_latex, expected_latex=expected_latex, options=add)
-                if test_result != expected_result:
+                checks = parse_checks(options)
+                result = 'true'
+                for check in checks:
+                    test_result = check_func[check](input_latex, expected_latex=expected_latex, options=checks[check])
+                    if test_result != 'true':
+                        result = test_result
+                        break
+                    
+                if result != expected_result:
                     fails.append(row + [test_result])
                     fail_count += 1
+                    break
                 test_count += 1
         
         report_str = '{} checks passed, {} failed\n'.format(test_count - fail_count, fail_count)
@@ -169,23 +115,10 @@ def test(event, context):
             })
         }
         return response
+#x = json.dumps({"input":"1 6/8","expected" : "1 #3/4","checks":"equivSymbolic;isSimplified;equivSyntax:isMixedFraction"}
+#)
+#res = handle({"body":x},{})
+#print(res)
 
-# res = handle({
-#     "body": "{\"input\": \"\\\\frac{1}{2}\", \"expected\": \"0.5\", \"checks\": \"equivLiteral:\"}"
-# }, {})
-# print(res)
-
-# res = test({}, {})
-# print(res)
-
-# res = calculate({
-#     "body": "{ \
-#         \"latex\": \"x+y+z+w\", \
-#         \"variables\": [ \
-#             { \"id\": \"x\", \"value\": 3 }, \
-#             { \"id\": \"y\", \"value\": 5 }, \
-#             { \"id\": \"z\", \"value\": \"x+y\" } \
-#         ] \
-#     }"
-# }, {})
-# print(res)
+res = test({}, {})
+print(res)
